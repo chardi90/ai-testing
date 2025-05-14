ae54e4d03c8532853a176eff29e6d0fbfefb9f7e
chardi90 <chardi90@hotmail.com>
2025-05-14 11:42:57 +0100

    Added AXIOS to HTML, linked JS file, added basic APIURL to test funtionality

---
+++ src/index.js
@@ -0,0 +1,7 @@
+let apiKey = "a2t477eebb3f98daaa0d6cf85ob51907";
+let prompt = "What is the best country to visit?";
+let context =
+  "Please give me one country that is safe, fun, cheap has great food and a warm climate.";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+console.log(apiUrl);
