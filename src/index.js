9abf8a4715824202ff310a09ae548027deb948fc
chardi90 <chardi90@hotmail.com>
2025-05-14 11:46:43 +0100

    Added getAxios command and showAnswer function to JS
    Checked that the apiUrl test I had created would console log properly with the help of axios

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+function showAnswer(response) {
+  console.log(response);
+}
+
 let apiKey = "a2t477eebb3f98daaa0d6cf85ob51907";
 let prompt = "What is the best country to visit?";
 let context =
   "Please give me one country that is safe, fun, cheap has great food and a warm climate.";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-console.log(apiUrl);
+axios.get(apiUrl).then(showAnswer);
